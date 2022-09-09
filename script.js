92b231d8e6ecf40d615023091bb57dc997ce2117
Lucas Biz Teixeira <lucaasbizteixeira@gmail.com>
2022-09-09 17:12:46 -0300

    atts

---
+++ script.js
@@ -0,0 +1,13 @@
+const men = document.getElementById('nav')
+const opaco = document.getElementById('main')
+
+function menu(){
+    if (men.style.display == 'none') {
+        men.style.display = 'block'
+        
+    } else {
+        men.style.display = 'none'
+
+
+    }
+}
