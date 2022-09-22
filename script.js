72518d291e20c173e63236aeaf3e24fddafb5929
Lucas Biz Teixeira <lucaasbizteixeira@gmail.com>
2022-09-22 16:16:45 -0300

    continuando a construcao

--- script.js
+++ script.js
@@ -1,18 +1,59 @@
 const men = document.getElementById('nav')
-const anim = document.getElementById('animation')
+const html = document.getElementById('txthtml')
+const css = document.getElementById('txtcss')
+const js = document.getElementById('txtjs')
+const react = document.getElementById('txtreact')
+const office = document.getElementById('txtoffice')
+
 
 
 function menu(){
     if (men.style.display == 'none') {
-        men.style.display = 'block'
-        
+        men.style.display = 'block'  
     } else {
         men.style.display = 'none'
-
-
     }
 }
 
-function fontchange(){
-    
+
+
+
+function showhtml(){
+    if (html.style.display == 'none') {
+        html.style.display = 'block'  
+    } else {
+        html.style.display = 'none'
+    }
+}
+
+function showcss(){
+    if (css.style.display == 'none') {
+        css.style.display = 'block'  
+    } else {
+        css.style.display = 'none'
+    }
+}
+
+function showjs(){
+    if (js.style.display == 'none') {
+        js.style.display = 'block'  
+    } else {
+        js.style.display = 'none'
+    }
+}
+
+function showreact(){
+    if (react.style.display == 'none') {
+        react.style.display = 'block'  
+    } else {
+        react.style.display = 'none'
+    }
+}
+
+function showoffice(){
+    if (office.style.display == 'none') {
+        office.style.display = 'block'  
+    } else {
+        office.style.display = 'none'
+    }
 }
