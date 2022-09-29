c8a6c5b71fa730a47c9e970b21e55a54a3a694c9
Lucas Biz Teixeira <lucaasbizteixeira@gmail.com>
2022-09-29 18:10:23 -0300

    toques finais

--- script.js
+++ script.js
@@ -55,6 +55,37 @@ function showoffice(){
     }
 }
 
+
+
 function easteregg(){
     alert('Olá! Nesta página existe um Easter Egg, será que você consegue encontrar sem olhar o código fonte? \u{1F440} ')
+}
+
+
+//SCROLL SUAVE
+
+const menuitens = document.querySelectorAll('#nav a[href^="#"')
+
+menuitens.forEach(item => {
+    item.addEventListener('click', scrollonclick)
+})
+
+function scrollonclick(event) {
+    event.preventDefault()
+    const element = event.target
+    const section = getscrollontop (event.target) - 80
+    scrolltoposition(section)
+
+}
+
+function scrolltoposition(section){
+    window.scroll({
+        top: section,
+        behavior: "smooth"
+    })
+}
+
+function getscrollontop(element){
+    const id = element.getAttribute('href')
+    return document.querySelector(id).offsetTop
 }
