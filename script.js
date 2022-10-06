f2cf281df19e4b47a54aa6517870accc9d2a1317
Lucas Biz Teixeira <lucaasbizteixeira@gmail.com>
2022-10-06 13:42:25 -0300

    atualizações no código

--- script.js
+++ script.js
@@ -5,61 +5,16 @@ const js = document.getElementById('txtjs')
 const react = document.getElementById('txtreact')
 const office = document.getElementById('txtoffice')
 
-
-
-function menu(){
-    if (men.style.display == 'none') {
-        men.style.display = 'block'  
+const show = (variavel) => {
+    if (variavel.style.display == 'none'){
+        variavel.style.display = 'block'
     } else {
-        men.style.display = 'none'
+        variavel.style.display = 'none'
     }
 }
 
-function showhtml(){
-    if (html.style.display == 'none') {
-        html.style.display = 'block'  
-    } else {
-        html.style.display = 'none'
-    }
-}
-
-function showcss(){
-    if (css.style.display == 'none') {
-        css.style.display = 'block'  
-    } else {
-        css.style.display = 'none'
-    }
-}
-
-function showjs(){
-    if (js.style.display == 'none') {
-        js.style.display = 'block'  
-    } else {
-        js.style.display = 'none'
-    }
-}
-
-function showreact(){
-    if (react.style.display == 'none') {
-        react.style.display = 'block'  
-    } else {
-        react.style.display = 'none'
-    }
-}
-
-function showoffice(){
-    if (office.style.display == 'none') {
-        office.style.display = 'block'  
-    } else {
-        office.style.display = 'none'
-    }
-}
-
-
-
-function easteregg(){
+const easteregg = () =>
     alert('Olá! Nesta página existe um Easter Egg, será que você consegue encontrar sem olhar o código fonte? \u{1F440} ')
-}
 
 
 //SCROLL SUAVE
